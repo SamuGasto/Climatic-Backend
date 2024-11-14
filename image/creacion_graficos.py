dcdcba7edb091d2ed3861e95d50f05a0f6b6144d
SamuGasto <samuel.55321@gmail.com>
2024-11-14 13:23:00 -0300

    quitando el print de creacion_graficos

--- image/creacion_graficos.py
+++ image/creacion_graficos.py
@@ -6,8 +6,6 @@ import xarray
 import os
 import io
 
-print(os.getcwd()+"/image/Mapa_REGION_border-Photoroom.png")
-
 era5 = xarray.open_zarr(
     "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2",
     chunks={'time': 48},
