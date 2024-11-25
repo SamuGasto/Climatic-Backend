ac88a4e4888c357f0d0fb5357e8a6977b4edae18
SamuGasto <samuel.55321@gmail.com>
2024-11-25 12:54:09 -0300

    fix error final

--- proceso_generacion_respuesta/generar_imagen.py
+++ proceso_generacion_respuesta/generar_imagen.py
@@ -17,18 +17,16 @@ def GenerarImagen(dataset, data1, data2, typechart, targetUnit):
         print("[GI] Generando gráfico vectorial...")
         tuplaList = JuntarValoresTupla(data1,data2)
         buffer = Vectorial_plot(tuplaList,dataset) #AQUI EL DATASET ES UN ARREGLO DE TUPLAS
-        #buffer = Polar_plot(tuplaList,dataset, targetUnit) #AQUI EL DATASET ES UN ARREGLO DE TUPLAS
         image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
         print("[GI] ¡Listo! retornando...")
         return image_base64
     elif (typechart == "polares"):
         print("[GI] Generando gráfico polar...")
         tuplaList = JuntarValoresTupla(data1,data2)
-        buffer = Polar_plot(tuplaList,dataset) #AQUI EL DATASET ES UN ARREGLO DE TUPLAS
+        buffer = Polar_plot(tuplaList,dataset, targetUnit) #AQUI EL DATASET ES UN ARREGLO DE TUPLAS
         image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
         print("[GI] ¡Listo! retornando...")
         return image_base64
-        pass
     else:
         print("[GI] No generando imagen, volviendo...")
         return None
